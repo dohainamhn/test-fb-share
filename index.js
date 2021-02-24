7071e887d54d1c3870ee00a7665eb8df21f22047
dohainam <dohainamhn@gmail.com>
2021-02-24 11:23:57 +0700

    m

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-
-
-
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function () {
   FB.init({
     appId: '1103333643516955',
     autoLogAppEvents: true,
     xfbml: true,
-    version: 'v9.0'
+    version: 'v9.0',
   });
-};  
+};
 
-const title = document.getElementById('title')
-function handle(response){
-  title.innerHTML = 'shared'
+const title = document.getElementById('title');
+function handle(response) {
+  title.innerHTML = 'shared';
 }
 function handleClick() {
   FB.ui(
     {
-      method: 'share',
-      href: 'https://developers.facebook.com/docs/',
+      method: 'share_open_graph',
+      action_type: 'og.shares',
+      action_properties: JSON.stringify({
+        object: 'http://www.heppiface.com/doodles/',
+      }),
     },
-    // callback
-    function(response) {
+    function (response) {
       if (response && !response.error_message) {
         alert('Posting completed.');
       } else {
         alert('Error while posting.');
       }
-    }
+    },
   );
 }
